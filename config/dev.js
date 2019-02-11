cfd116de92295155d33f3c5228eed8962512ce01
bmshamsnahid <b.m.shamsnahid@gmail.com>
2019-02-12 00:13:59 +0600

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '191095326968-t27mar7i704lud9009thdkeok6e286o6.apps.googleusercontent.com',
+  googleClientSecret: 'XVfmrtZsibzKbhoOWDyr1Q2d',
+  mongoURI: 'mongodb://dbuser:dbpassword56316498@ds241664.mlab.com:41664/survey-mail-dev',
+  cookieKey: 'myCookieKey',
+};
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,8 @@
-module.exports = {
-  googleClientID: '191095326968-t27mar7i704lud9009thdkeok6e286o6.apps.googleusercontent.com',
-  googleClientSecret: 'XVfmrtZsibzKbhoOWDyr1Q2d',
-  mongoURI: 'mongodb://dbuser:dbpassword56316498@ds241664.mlab.com:41664/survey-mail-dev',
-  cookieKey: 'myCookieKey',
-};
+const devConfig = require('./dev');
+const prodConfig = require('./prod');
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = prodConfig;
+} else {
+  module.exports = devConfig;
+}
