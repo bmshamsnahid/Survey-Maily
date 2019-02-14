6cb39e579f4994b2ed97e6272d0a6e1322f7f801
bmshamsnahid <b.m.shamsnahid@gmail.com>
2019-02-14 16:19:45 +0600

    test gitIgnore 03

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID: '191095326968-t27mar7i704lud9009thdkeok6e286o6.apps.googleusercontent.com',
-  googleClientSecret: 'XVfmrtZsibzKbhoOWDyr1Q2d',
-  mongoURI: 'mongodb://dbuser:dbpassword56316498@ds241664.mlab.com:41664/survey-mail-dev',
-  cookieKey: 'myCookieKey',
-  stripePublishableKey: 'pk_test_WrUREODJMeeD71YVWPChhOOM',
-  stripeSecretKey: 'sk_test_MKZR4fUwXkOPtt360LswDASw',
-  sendGridKey: '',
-};
